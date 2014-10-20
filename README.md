2a3dfaac70a5de137ddf89db182007ee6b64fe0f
Stephen Parker <web@withaspark.com>
2014-10-19 23:25:35 -0400

    Initial commit.

--- README.md
+++ README.md
@@ -1,4 +1,46 @@
-1page-gallery
+1Page Gallery
 =============
 
-An auto-generated, simple, single-page jQuery, PHP photo gallery from images in a directory.
+An auto-generated, simple, single-page jQuery, HTML5, CSS3, PHP photo gallery from a zip file of images.
+
+1Page Gallery is simply the easiest photo gallery to deploy for non-programmers and developers needing to give clients a simple photo gallery with minimal footprint. The 1Page Gallery is a jQuery, HTML5, CSS3, PHP slideshow gallery which will automatically create a simple, elegant desktop and mobile-ready image gallery.
+
+Simply add the index.php, build script, and a zip of your high resolution images and a simple,
+elegant jQuery, HTML5, CSS3, PHP photo gallery will be auto-magically generated.
+
+Free for all purposes, personal and commercial under the MIT license. See [LICENSE](LICENSE). Attribution in the footer is certainly appreciated, but not required; however, license text must remain intact on index.php.
+
+
+
+###Dependencies:###
+- php5
+- imagemagick (to use build script)
+- unzip
+
+To install dependencies,
+
+```sh
+sudo apt-get update && sudo apt-get install php5 imagemagick unzip
+```
+or
+```sh
+sudo yum update && sudo yum install php5 imagemagick unzip
+```
+
+###Installation:###
+1. Clone/checkout this repo
+2. Place a zip file containing your high resolution images at the same level as index.php
+3. Run build script from same directory as zip file to generate thumbnails and reasonably small images
+   ```sh
+   ./build
+   ```
+
+###Configuration###
+- In index.php
+```php
+// Heading to title page
+$siteName = '1Page Gallery';
+
+// Duration to display each slide in milliseconds
+$slideTime = 5000;
+```
---
+++ index.php
@@ -0,0 +1,365 @@
+<?php
+/*
+The MIT License (MIT)
+
+Copyright (c) 2014 Stephen Parker
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+
+
+////////////////////////////////////
+// CONFIGURATION
+////////////////////////////////////
+
+// Heading to title page
+$siteName = '1Page Gallery';
+
+// Duration to display each slide in milliseconds
+$slideTime = 5000;
+
+// Your logo; leave blank if wish not to use one
+$logo = '';
+
+// END OF CONFIGURATION
+
+
+
+// Location of zip file containing all images for download
+$zip = '';
+
+// Array of all images
+$images = array();
+
+// Array of all thumbnails
+$thumbs = array();
+
+// Directory images were generated to; this should match build script
+$dir = 'images/';
+
+// Directory thumbnails were generated to; this should match build script
+$thumbDir = 'thumbs/';
+
+// Get zip of all images if available
+if ($handle = opendir(getcwd())) {
+	while (($file = readdir($handle)) !== false) {
+		if (strstr(strtolower($file), '.zip')) {
+			$zip = $file;
+			break;
+		}
+	}
+	closedir($handle);
+}
+
+// Get all images in the directory
+if ($handle = opendir($dir)) {
+	while (($image = readdir($handle)) !== false) {
+		if (
+			strstr(strtolower($image), '.jpg') ||
+			strstr(strtolower($image), '.jpeg')
+		) {
+			// If thumbnail exists, add
+			if (file_exists($thumbDir.$image)) {
+				$images[] = $dir . $image;
+				$thumbs[] = $thumbDir . $image;
+			}
+		}
+	}
+	closedir($handle);
+}
+?>
+<!doctype html>
+<html lang='en'>
+<head>
+	<meta charset='utf-8'>
+	<title><?php echo $siteName; ?></title>
+
+	<script src='//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'></script>
+	<link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+	<style type='text/css'>
+	body {
+		padding: 1em;
+		background-color: #444;
+		color: #fff;
+		font-family: 'Montserrat', Verdana, sans-serif;
+		font-size: 0.8em;
+	}
+	h1 {
+		margin: 0em 0em 0.5em 0em;
+		font-size: 2em;
+		font-weight: normal;
+		text-align: center;
+	}
+	h1 img {
+		max-width: 33%;
+		min-width: 200px;
+	}
+	a, a:hover, a:link, a:visited, a:active {
+		color: #ddd;
+		outline: 0;
+	}
+	a:hover {
+		text-decoration: none;
+	}
+	.clear { clear: both; }
+	#page {
+		max-width: 700px;
+		margin: auto auto;
+		padding: 1em;
+		opacity: 0.9;
+		background-color: #222;
+	}
+	#nav {
+		margin: 0.5em auto;
+		text-align: right;
+	}
+	#footer {
+		max-width: 700px;
+		margin: auto auto;
+		padding: 1em;
+		opacity: 0.7;
+	}
+	#stage {
+		position: relative;
+		width: 100%;
+		max-height: 450px;
+	}
+	#stage img {
+		width: 100%;
+		height: auto;
+		max-height: 450px;
+	}
+	#stage .control {
+		position: absolute;
+		width: 50px;
+		top: 0px;
+		bottom: 0px;
+		font-size: 3em;
+		text-align: center;
+		background-color: #222;
+		opacity: 0;
+		cursor: pointer;
+	}
+	#stage .control.show {
+		opacity: 0.3;
+	}
+	#stage .control.hover {
+		opacity: 0.7;
+	}
+	.control.previous {
+		left: 0px;
+		right: auto;
+	}
+	.control.next {
+		left: auto;
+		right: 0px;
+	}
+	.control a {
+		position: absolute;
+		top: 50%;
+		width: 100%;
+		margin-top: -50%;
+		margin-left: -50%;
+		text-decoration: none;
+		vertical-align: middle;
+	}
+	#selector {
+		position: relative;
+		width: 100%;
+		height: 5em;
+		margin: 1em 0em;
+		padding: 0em;
+		list-style: none;
+		overflow-x: scroll;
+		overflow-y: hidden;
+		white-space: nowrap;
+	}
+	.thumbnail {
+		display: inline-block;
+		margin-left: 0px;
+	}
+	.thumbnail img {
+		height: 3.5em;
+		width: auto;
+		opacity: 0.2;
+		cursor: pointer;
+	}
+	.currentThumb, .thumbnail img:hover {
+		opacity: 1.0 !important;
+	}
+	</style>
+</head>
+<body>
+<meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1'>
+
+	<div id='page'>
+		<h1>
+<?php
+		if ($logo != '')
+			echo "			<img src='$logo' alt='$siteName'>";
+		else
+			echo "			$siteName";
+?>
+		</h1>
+
+		<div id='slideshow'>
+			<div id='stage'>
+				<img id='centerStage' src='#' alt='Gallery Image'>
+				<div class='control previous'><a class='previous' href='#'>&lt;</a></div>
+				<div class='control next'><a class='next' href='#'>&gt;</a> </div>
+				<div class='clear'> </div>
+			</div>
+			<ul id='selector'>
+<?php
+$counter = 0;
+foreach ($thumbs as $thumb) {
+	echo "				<li class='thumbnail'><img class='changer' id='changeTo$counter' src='$thumb' alt='Gallery Thumbnail'></li>\n";
+	$counter++;
+}
+?>
+			</ul>
+			<div class='clear'> </div>
+		</div>
+
+<?php
+			if ($zip != "") {
+				echo "		<div id='nav'><a href='$zip'>Download All Images</a></div>";
+			}
+?>
+		<div class='clear'> </div>
+
+	</div>
+
+	<div id='footer'>
+		A <a href='https://github.com/withaspark/1page-gallery'>1Page Gallery</a> by <a href='http://withaspark.com'>Stephen Parker</a>.
+	</div>
+
+
+
+	<script type='text/javascript'>
+	var curImage = 0;
+	var autoPlay = true;
+	var timer = 0;
+	var images = [
+<?php
+foreach ($images as $image)
+	echo "		'$image',\n";
+?>
+	];
+
+	// Load the first image
+	ChangeTo(0);
+
+	// Poll every second to display new image
+	setInterval(function() {
+		if (autoPlay) {
+			timer = timer + 1000;
+			if (timer > <?php echo $slideTime; ?>) {
+				timer = 0;
+				NextImage();
+			}
+		}
+	}, 1000);
+
+	// Clicking the main image will advance to the next image
+	$('#centerStage').click(function() {
+		NextImage();
+	});
+
+	// Clicking a thumbnail image will change to it
+	$('.changer').click(function() {
+		var index = $(this).attr('id').replace(/ChangeTo/gi, '');
+		ChangeTo(index);
+	});
+
+	// Hovering over center stage shows next and previous links
+	$('#centerStage').parent().hover(function() {
+		$('.control').addClass('show');
+	}, function() {
+		$('.control').removeClass('show');
+	});
+
+	// Hovering over control accents next and previous links
+	$('.control').hover(function() {
+		$(this).addClass('hover');
+	}, function() {
+		$(this).removeClass('hover');
+	});
+
+	// If click any elements with next class advance
+	$('.next').click(function() {
+		NextImage();
+		return false;
+	});
+
+	// If click any elements with previous class go back
+	$('.previous').click(function() {
+		PreviousImage();
+		return false;
+	});
+
+	// Function to change to the next image
+	function NextImage() {
+		autoplay = true;
+		timer = 0;
+		if (curImage + 1 < images.length)
+			curImage++;
+		else
+			curImage = 0;
+		ChangeTo(curImage);
+	}
+
+	// Function to change to the previous image
+	function PreviousImage() {
+		autoplay = false;
+		timer = 0;
+		if (curImage > 0)
+			curImage--;
+		else
+			curImage = images.length - 1;
+		ChangeTo(curImage);
+	}
+
+	// Function to change to a specific image
+	function ChangeTo(index) {
+		var elpos = $('#changeTo' + index).position().left;
+		var fadeDuration = 400;
+		autoplay = true;
+		timer = 0;
+		curImage = parseInt(index);
+		$('#centerStage').fadeOut(fadeDuration);
+		setTimeout(function() {
+			$('#centerStage').attr('src', images[index]).fadeIn();
+		}, fadeDuration);
+		$('.changer').removeClass('currentThumb');
+		$('#changeTo' + index).addClass('currentThumb');
+		$('#selector').scrollLeft($('.changer').outerWidth() * index);
+	}
+
+	// Function to preload images
+	preload(images);
+	function preload(imageArray) {
+		$(imageArray).each(function() {
+			$('<img/>').attr('src', this).appendTo('body').hide();
+		});
+	}
+	</script>
+</body>
+</html>
